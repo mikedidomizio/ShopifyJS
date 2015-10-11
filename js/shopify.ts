5d2ecab409ca4959c5f354cc1aee51208a1fef06
Mike DiDomizio <mikedidomizio@gmail.com>
2015-10-11 13:50:25 -0400

    - Created Typescript Shopify file - Changed Shopify JS Object into Class - Renamed espresso.js to espresso.json - Updated example.html to have promises instead of callbacks - Updated parameter descriptions for methods

--- js/shopify.ts
+++ js/shopify.ts
@@ -1,6 +1,6 @@
 "use strict";
 /**
- *
+ * 
  * Shopify Javascript API
  * Retrieves ATOM feeds in expected Shopify format
  *
@@ -12,16 +12,15 @@
  *
  */
 class Shopify {
+    website: string;
     /**
      * Constructor
      *
      * @param host|undefined    Domain to call, leaving empty will make local AJAX calls
      */
-    constructor(host) {
-        this.host = host;
+    constructor(public host: string) {
         this.website = host ? host.trim() : "";
-    }
-    ;
+    };
     /**
      * Makes an AJAX call to a feed url
      *
@@ -29,96 +28,129 @@ class Shopify {
      * @param retrieveArray     Array   An array of keys of items that we will return
      * @returns Object
      */
-    ajax(url, retrieveArray) {
-        var self = this, itemsToRetrieve = retrieveArray instanceof Array ? retrieveArray : [];
-        return new Promise(function (resolve, reject) {
-            var feedURL = self.website + url.trim(), xmlnsS = "http://jadedpixel.com/-/spec/shopify";
-            if (feedURL.slice(-4).toLowerCase() === 'json') {
-                var dataType = "json";
+    ajax(url: string, retrieveArray: Array<string>): Object {
+        var self = this,
+            itemsToRetrieve: Array<string> = retrieveArray instanceof Array ? retrieveArray : [];
+
+        return new Promise(function(resolve, reject) {
+
+            var feedURL:string = self.website + url.trim(),
+                xmlnsS:string = "http://jadedpixel.com/-/spec/shopify";
+
+            if(feedURL.slice(-4).toLowerCase() === 'json') {
+                var dataType: string = "json"
+            } else {
+                var dataType: string = feedURL.match(/\&|\?callback\=\?/) ? 'jsonp' : 'xml';
             }
-            else {
-                var dataType = feedURL.match(/\&|\?callback\=\?/) ? 'jsonp' : 'xml';
-            }
+
             var xmlHttp = new XMLHttpRequest();
-            xmlHttp.onreadystatechange = function () {
+
+            xmlHttp.onreadystatechange = function() {
                 if (xmlHttp.readyState === 4) {
-                    if (xmlHttp.status === 200) {
-                        var data = xmlHttp.responseText, obj = [];
+                    if(xmlHttp.status === 200) {
+
+                        var data = xmlHttp.responseText,
+                            obj = [];
+
                         //is it coming back in json format?
-                        if (dataType == 'json') {
+                        if(dataType == 'json') {
+
                             // convert it to an object
                             data = JSON.parse(data);
-                            if (itemsToRetrieve.length > 0) {
-                                var item = {};
-                                for (var i in data) {
-                                    if (itemsToRetrieve.indexOf(i) != -1) {
+
+                            if(itemsToRetrieve.length > 0) {
+                                var item: any = {};
+
+                                for(var i in <any>data) {
+                                    if(itemsToRetrieve.indexOf(i) != -1) {
                                         item[i] = data[i];
                                     }
                                 }
+
                                 obj.push(item);
-                            }
-                            else {
+                            } else {
                                 obj.push(data);
                             }
-                        }
-                        else {
+                        } else {
+
                             //if it is a jsonp call, ensure that you know what data you'll be getting back
-                            if (dataType === 'jsonp') {
-                                var data = decodeURIComponent(data);
+                            if(dataType === 'jsonp') {
+                                var data:string = decodeURIComponent(data);
                             }
+
                             //there is always a chance for things to go bad with XML
                             var parser = new DOMParser();
                             var xmlDoc = parser.parseFromString(data, "text/xml");
-                            for (var i in xmlDoc) {
+
+                            for(var i in <any>xmlDoc) {
+
                                 try {
-                                    if (typeof xmlDoc[i] == "object") {
-                                        for (var j in xmlDoc[i]) {
-                                            if (typeof xmlDoc[i][j] === "string") {
+
+                                    if(typeof xmlDoc[i] == "object") {
+
+                                        for(var j in <any>xmlDoc[i]) {
+
+                                            if(typeof xmlDoc[i][j] === "string") {
+
                                                 var dom = parser.parseFromString(xmlDoc[i][j], "text/xml");
                                                 var entries = dom.documentElement.getElementsByTagName("entry");
-                                                for (var e in entries) {
-                                                    if (entries[e].nodeName === "entry") {
+
+                                                for(var e in <any>entries) {
+                                                    if(entries[e].nodeName === "entry") {
                                                         var entry = entries[e];
-                                                        var item = {};
+
+                                                        var item: any = {};
+
                                                         item.title = entry.getElementsByTagName("title")[0].childNodes[0].nodeValue;
                                                         item.published = entry.getElementsByTagName("published")[0].childNodes[0].nodeValue;
                                                         item.updated = entry.getElementsByTagName("updated")[0].childNodes[0].nodeValue;
-                                                        if (itemsToRetrieve.indexOf("summary") != -1) {
+
+                                                        if(itemsToRetrieve.indexOf("summary") != -1) {
                                                             item.summary = entry.getElementsByTagName("summary")[0].childNodes[0].nodeValue;
                                                         }
-                                                        var all = entry.getElementsByTagNameNS(xmlnsS, "*");
-                                                        for (var i in all) {
-                                                            switch (all[i].nodeName) {
-                                                                case "s:tag":
-                                                                case "s:type":
-                                                                case "s:vendor":
-                                                                    if (all[i].firstChild.nodeValue != "" && (itemsToRetrieve.length == 0 || itemsToRetrieve.indexOf(all[i].nodeName) != -1)) {
+
+                                                        var all = entry.getElementsByTagNameNS(xmlnsS,"*");
+
+                                                        for(var i in all) {
+
+                                                            switch(all[i].nodeName) {
+                                                                case "s:tag" :
+                                                                case "s:type" :
+                                                                case "s:vendor" :
+
+                                                                    if(all[i].firstChild.nodeValue != "" && (itemsToRetrieve.length == 0 || itemsToRetrieve.indexOf(all[i].nodeName) != -1)) {
+
                                                                         var tagName = all[i].tagName.match(/^s\:([\w]+)$/);
-                                                                        if (all[i].nodeName == "s:vendor") {
+
+                                                                        if(all[i].nodeName == "s:vendor") {
                                                                             item[tagName[1]] = all[i].firstChild.nodeValue;
-                                                                        }
-                                                                        else {
-                                                                            if (!item[tagName[1]]) {
+                                                                        }else {
+                                                                            if(!item[tagName[1]]) {
                                                                                 item[tagName[1]] = new Array();
                                                                             }
+
                                                                             item[tagName[1]].push(all[i].firstChild.nodeValue);
                                                                         }
                                                                     }
                                                                     break;
-                                                                case "s:variant":
-                                                                    if (itemsToRetrieve.length == 0 || itemsToRetrieve.indexOf("s:variant") != -1) {
+                                                                case "s:variant" :
+                                                                    if(itemsToRetrieve.length == 0 || itemsToRetrieve.indexOf("s:variant") != -1) {
                                                                         var variant = {};
-                                                                        if (typeof item.variant == "undefined") {
+
+                                                                        if(typeof item.variant == "undefined") {
                                                                             item.variant = [];
                                                                         }
-                                                                        for (var k = 1; k < 11; k += 2) {
+
+                                                                        for(var k = 1; k < 11; k += 2) {
                                                                             variant[all[i].childNodes[k].nodeName] = all[i].childNodes[k].nodeValue;
                                                                         }
+
                                                                         item.variant.push(variant);
                                                                     }
                                                                     break;
                                                             }
                                                         }
+
                                                         // Adds the item to the object
                                                         obj.push(item);
                                                     }
@@ -126,23 +158,25 @@ class Shopify {
                                             }
                                         }
                                     }
-                                }
-                                catch (err) {
+
+                                } catch(err) {
                                     // error occurred when trying to parse an entry
                                     // do not rejecr
-                                    console.warn(err);
+                                    console.warn(err)
                                 }
                             }
                         }
+
                         resolve(obj);
-                    }
-                    else {
+                    } else {
                         reject(xmlHttp.responseText);
                     }
                 }
-            };
+            }
+
             xmlHttp.open("GET", feedURL, true);
             xmlHttp.send(null);
-        });
+
+        })
     }
 }
